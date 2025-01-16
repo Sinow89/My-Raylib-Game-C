03eee527c6bdaf44346bcfa8663aa01ae4595f59
Sinow89 <christoffer.rozenbachs@gmail.com>
2025-01-16 15:18:32 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,34 +8,46 @@ int main(void) {
     // Initialize the window
     InitWindow(800, 600, "Raylib - Hello World");
     SetTargetFPS(60);
-        int xpos = 400;
-        int ypos = 300;
+        float xpos = 400;
+        float ypos = 300;
+        float speed = 5;
     
     while (!WindowShouldClose()) {  
  
+        
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawText("Hello, Raylib!", 10, 10, 20, LIGHTGRAY);
+        DrawCircle(200, 200, 10, BLUE);
         DrawRectangle(xpos, ypos, 75, 25, WHITE);
 
+        Vector2 center = {xpos , ypos};
+        Rectangle rec = {200 , 200  , 10, 10};
+
+        
+        if(CheckCollisionCircleRec(center, 2.0f, rec))
+        {
+            DrawText("Hit!", 10, 10, 20, LIGHTGRAY);
+        }
+
+        
         if(IsKeyDown(KEY_W))
         {
-            ypos = ypos-10;
+            ypos = ypos-speed;
         }
 
         if(IsKeyDown(KEY_A))
         {  
-            xpos = xpos-10;
+            xpos = xpos-speed;
         }
 
         if(IsKeyDown(KEY_S))
         {
-            ypos = ypos+10;        
+            ypos = ypos+speed;        
         }
 
         if(IsKeyDown(KEY_D))
         {
-            xpos = xpos+10;
+            xpos = xpos+speed;
         }
         
         DrawFPS(700, 500);
