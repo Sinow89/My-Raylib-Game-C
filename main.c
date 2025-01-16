367b8a3afa1872740f411a64ccf145e52e6e763c
Sinow89 <christoffer.rozenbachs@gmail.com>
2025-01-16 10:09:56 +0100

    Added

--- main.c
+++ main.c
@@ -8,13 +8,22 @@ int main(void) {
     // Initialize the window
     InitWindow(800, 600, "Raylib - Hello World");
     SetTargetFPS(60);
-
+    
 
     while (!WindowShouldClose()) {  
+        int xpos = 400;
+        int ypos = 300;
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Hello, Raylib!", 10, 10, 20, LIGHTGRAY);
-        DrawCircle(400, 300, 10, GREEN);
+        DrawRectangle(xpos, ypos, 75, 25, GREEN);
+
+        if(IsKeyDown(KEY_W))
+        {
+            xpos+10;
+        }
+
+        DrawFPS(700, 500);
         EndDrawing();
     }
 
