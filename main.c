3cb3c4a9da85ad7bc5b40898e98a0a64b8cb3ec3
Sinow89 <christoffer.rozenbachs@gmail.com>
2025-01-20 14:14:45 +0100

    Same, refactored and made a player struct, and changed main accordingly.

--- main.c
+++ main.c
@@ -7,35 +7,37 @@ zig cc -I"C:/raylib/include" -L"C:/raylib/lib" -o "MyGame.exe" main.c -lraylib -
 */
 
 typedef struct{
-        float ball_xpos;
-        float ball_ypos;
-        float ball_speed;
-        float ball_radius
+    float ball_xpos;
+    float ball_ypos;
+    float ball_speed;
+    float ball_radius;
 } ball;
 
-
+typedef struct{
+    float player_xpos;
+    float player_ypos;
+    float player_speed;
+} player;
 
 int main(void) {
     // Initialize the window
     InitWindow(800, 600, "Raylib - Hello World");
     SetTargetFPS(60);
-        float player_xpos = 400;
-        float player_ypos = 300;
-        float player_speed = 5;
 
-        ball game_ball = {200, 200, 2, 10};
+    player game_player = {400, 300, 5};
+    ball game_ball = {200, 200, 2, 10};
 
     while (!WindowShouldClose()) {
 
         BeginDrawing();
         ClearBackground(BLACK);
         DrawCircle(game_ball.ball_xpos, game_ball.ball_ypos, game_ball.ball_radius, BLUE);
-        DrawRectangle(player_xpos, player_ypos, 75, 25, WHITE);
+        DrawRectangle(game_player.player_xpos, game_player.player_ypos, 75, 25, WHITE);
 
         //This Vector2 is the center of the ball.
         //The Rectangle defines the Retangle and sets the position and size.
         Vector2 center = {game_ball.ball_xpos , game_ball.ball_ypos};
-        Rectangle rec = {player_xpos , player_ypos  , 75, 25};
+        Rectangle rec = {game_player.player_xpos , game_player.player_ypos  , 75, 25};
 
         while (1)
         {
@@ -55,24 +57,24 @@ int main(void) {
         /*----------------------Controls-------------------------*/
         /*-------------------------------------------------------*/
 
-        if(IsKeyDown(KEY_W) && player_ypos > 0)
+        if(IsKeyDown(KEY_W) && game_player.player_ypos > 0)
         {
-            player_ypos = player_ypos-player_speed;
+            game_player.player_ypos = game_player.player_ypos-game_player.player_speed;
         }
 
-        if(IsKeyDown(KEY_A) && player_xpos > 0)
+        if(IsKeyDown(KEY_A) && game_player.player_xpos > 0)
         {
-            player_xpos = player_xpos-player_speed;
+            game_player.player_xpos = game_player.player_xpos-game_player.player_speed;
         }
 
-        if(IsKeyDown(KEY_S) && player_ypos < 575)
+        if(IsKeyDown(KEY_S) && game_player.player_ypos < 575)
         {
-            player_ypos = player_ypos+player_speed;
+            game_player.player_ypos = game_player.player_ypos+game_player.player_speed;
         }
 
-        if(IsKeyDown(KEY_D) && player_xpos < 725)
+        if(IsKeyDown(KEY_D) && game_player.player_xpos < 725)
         {
-            player_xpos = player_xpos+player_speed;
+            game_player.player_xpos = game_player.player_xpos+game_player.player_speed;
         }
 
         DrawFPS(700, 500);
